344064c4f506e9e2f1451bfec1332357817e006f
bstharsh <bstharshdeals@gmail.com>
2025-07-14 22:19:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ async def details(update: Update, context: ContextTypes.DEFAULT_TYPE):
         return
     user_id = int(context.args[0])
     logs = collection.find({"user_id": user_id})
-    msg = f"📄 Logs for user ID {user_id}:
+    msg = f"📄 Logs for user ID {user_id}:"
 "
     found = False
     for log in logs:
